0069eb5978c666f5142cf7252d978ac577ef33e4
Gavaii <61867970+Gavaii@users.noreply.github.com>
2021-01-12 18:58:02 +0100

    Renamed files and classes according to class diagram

--- main/src/Application.java
+++ main/src/Application.java
@@ -1,2 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+import resources.ReadCSV;
+
 public class Application {
+    public static void main(String[] args) {
+        String delimiter=";";
+        String filepath="C:\\Users\\valen\\OneDrive\\Desktop\\Projekte\\CSVTEST.csv";
+
+        ReadCSV csvin=new ReadCSV();
+
+        csvin.readCmd(delimiter,filepath);
+        Double[][] table=csvin.getTable();
+
+        csvin.printTable();
+
+        csvin.printPoints();
+
+    }
 }
--- main/src/resources/ReadCSV.java
+++ main/src/resources/ReadCSV.java
@@ -1,35 +1,75 @@
 package resources;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
-public class CSV_Reader{
-    public ArrayList getTable() {
+public class ReadCSV{
+    private Double[][] table=new Double[5][5];
+    private String[] points=new String[5];
+
+    public Double[][] getTable() {
         return table;
     }
+    public String[] getPoints() { return points; }
 
-    private ArrayList table=new ArrayList<String>();
+    public void readCmd(String delimiter,String filepath){
 
-    public void readCsvCmd(String delimiter){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filepath))) {
 
+            // read the file line by line
+            String line;
+            int i=0,j=0,counter;
+            while ((line = br.readLine()) != null) {
 
-        //parsing a CSV file into Scanner class constructor
-        try
-        {
-            Scanner sc = new Scanner(new File("C:\\Users\\valen\\OneDrive\\Desktop\\Projekte\\CSVTEST.csv"));
-            sc.useDelimiter(delimiter);   //sets the delimiter pattern
-            while (sc.hasNext()){  //boolean value
-                System.out.print(sc.next()+"     ");  //find and returns the next complete token from this scanner
-                //table.add(sc.next());
+                // convert line into columns
+                String[] columns = line.split(delimiter);
+                if(isDouble(columns[0]) && isDouble(columns[1])){
+                    for (String c: columns) {
+                        table[j-1][i]=Double.parseDouble(c);
+                        i++;
+                    }
+                }else if(!isDouble(columns[0])&&isDouble(columns[1])){           //incase the csv-file has the points also written vertically
+                    for(i=1;i<columns.length-1;i++)
+                        table[j][(i)-1]=Double.parseDouble(columns[i]);
+
+                }else if(!isDouble(columns[0])&&!isDouble(columns[1])){     //incase the csv-file has point written at the first line (horizontally)
+                    counter = 0;
+                    for (String cities : columns) {
+                        points[counter] = cities;
+                        counter++;
+                    }
+                }
+                i=0;
+                j++;
             }
-            sc.close();  //closes the scanner
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        catch (FileNotFoundException ex)
-        {
-            System.err.println("File not found!");
+    }
+    boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public void printTable() {
+        for (Double[] doubles : table) { //this equals to the row in our matrix.
+            for (Double aDouble : doubles) { //this equals to the column in each row.
+                System.out.print(aDouble + "  ");
+            }
+            System.out.println(); //change line on console as row comes to end in the matrix.
+        }
+    }
+    public void printPoints() {
+        for (String city: points) {
+            System.out.println(city);
         }
     }
 }
