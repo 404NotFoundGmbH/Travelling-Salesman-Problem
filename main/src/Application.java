2426616fcff0ddc126d4188f28085c741e0f5f66
Silas Demez <silasdemez@gmail.com>
2021-01-27 21:21:14 +0100

    added extendedNearestneighbor function executes NearestNeighbor for every every point

--- main/src/Application.java
+++ main/src/Application.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
-
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 import resources.*;
 
@@ -12,26 +13,20 @@ public class Application {
     public static void main(String[] args) {
         String delimiter=";";
 
-        long startTime = System.nanoTime();
+        long startTime = Instant.now().toEpochMilli();
+
         ReadCSV csvin = new ReadCSV(delimiter);
         csvin.setFilepath();
         csvin.readCmd();
         double[][] table = csvin.getdTable();
 
-        //csvin.printTable();
-        //System.out.println(" ");
-
         NearestNeighborV2 nearestNeighbor = new NearestNeighborV2();
-        table = nearestNeighbor.executeNearestNeighbor(table,0);
-        //System.out.println(Arrays.deepToString(table));
-        //System.out.println("Distance: " + nearestNeighbor.getDistance(table));
-        nearestNeighbor.getDistance(table);
-        //csvin.printPoints();
+        System.out.println(nearestNeighbor.extendedNearestNeighbor(table));
 
-        long endTime = System.nanoTime();
+        long endTime = Instant.now().toEpochMilli();
 
         long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-        System.out.println("Execution time: " + duration/1000000 + "");
+        System.out.println("That took " + duration/2 + " milliseconds");
 
     }
 }
