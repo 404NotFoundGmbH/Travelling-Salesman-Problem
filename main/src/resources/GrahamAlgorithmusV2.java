73f48e2a8c9bf048266f2bad4b2c43014dc1a414
Gavaii <61867970+Gavaii@users.noreply.github.com>
2021-02-02 17:08:48 +0100

    Revert "Merge branch 'main' of https://github.com/404NotFoundGmbH/Travelling-Salesman-Problem into main"
    This reverts commit f3c596c9ac21f0aa54e92a9d956d5212803babf2, reversing
    changes made to dd07aa291629db77e8865cdbd5e7d7976960a305.

--- main/src/resources/GrahamAlgorithmusV2.java
+++ main/src/resources/GrahamAlgorithmusV2.java
@@ -1,4 +1,4 @@
-package resources;
+package grahamalgorithmus;
 
 import java.awt.Point;
 import java.util.*;
@@ -16,9 +16,8 @@ enum Direction {
  * and inserts the remaining nodes in order to solve the TSP (Traveling Salesman Problem)
  */
 public class GrahamAlgorithmusV2 {
-   public static List<Point> allNodes = new ArrayList<>();
+   private static List<Point> allNodes = new ArrayList<>();
    private static List<Point> sortedPoints;
-   public static double finalDistance = 0;
     /**
      * This function saves the given points into the list <code>allNodes</code>
      * @param points List of points which should be calculated
@@ -97,7 +96,7 @@ public class GrahamAlgorithmusV2 {
        List<Point> remainingNodes = new ArrayList<>(sortedPoints);
        remainingNodes.removeAll(convexHull);
 
-
+       double finalDistance = 0;
 
        for(int i = 0; i < remainingNodes.size(); i++){
            int firstIndex = 0;
--- main/src/websocket/EchoWebSocket.java
+++ main/src/websocket/EchoWebSocket.java
@@ -2,64 +2,12 @@ package websocket;
 
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
-
-import java.awt.*;
 import java.io.*;
 import java.util.*;
-import java.util.List;
 import java.util.concurrent.*;
-import resources.*;
-
-
 
 @WebSocket
 public class EchoWebSocket {
-
-        public String pointToJson(List<Point> points){
-            String string = "[";
-
-            for (Point i : points){
-                string = string.concat(i.x + "," + i.y + ",");
-            }
-
-            StringBuilder points1 = new StringBuilder(string);
-            points1.setCharAt(string.length()-1, ']');
-            return String.valueOf(points1);
-        }
-
-        public double[][] jsonToArray(String message) {
-            StringBuilder sb = new StringBuilder(message);
-            sb.deleteCharAt(message.length() - 1);
-            sb.deleteCharAt(0);
-            message = sb.toString();
-            String[] str = message.split(",");
-
-            double[] doubleValues = Arrays.stream(str)
-                    .mapToDouble(Double::parseDouble)
-                    .toArray();
-
-            //List<Point> points = new ArrayList<>();
-            double[][] points = new double[(doubleValues.length/2)][2];
-            List<Double> longi = new ArrayList<>();
-            List<Double> lat = new ArrayList<>();
-            //double[] longi;
-            //double[] lat;
-
-            for (int i=0;i<doubleValues.length;i++) {
-                if (i%2 == 0){
-                    longi.add(doubleValues[i]);
-                    lat.add(doubleValues[i+1]);
-                }
-            }
-
-            for (int i=0;i<longi.size();i++){
-                points[i][0] = longi.get(i).doubleValue();
-                points[i][1] = lat.get(i).doubleValue();
-            }
-
-            return points;
-        }
-
 
         // Store sessions if you want to, for example, broadcast a message to all users
         private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
@@ -81,23 +29,7 @@ public class EchoWebSocket {
             System.out.println("");
             System.out.println("");
             System.out.println("Got: " + message);   // Print message
-            double[][] test = jsonToArray(message);
-            NearestNeighborV2 nearestNeighborV2 = new NearestNeighborV2();
-            test =  nearestNeighborV2.executeNearestNeighbor(test,0);
-            System.out.println(Arrays.deepToString(test));
-            GrahamAlgorithmusV2.allNodes.add(new Point(32,27));
-            GrahamAlgorithmusV2.allNodes.add(new Point(20,11));
-            GrahamAlgorithmusV2.allNodes.add(new Point(50,43));
-            GrahamAlgorithmusV2.allNodes.add(new Point(24,53));
-
-            /*
-            JSONArray jsonArray = new JSONArray();
-            jsonArray.add(GrahamAlgorithmusV2.computeConvexHull());
-             */
-
-
-            session.getRemote().sendString(String.valueOf(pointToJson(GrahamAlgorithmusV2.computeConvexHull()))); // sends the into json format converted points
-            session.getRemote().sendString(String.valueOf(GrahamAlgorithmusV2.finalDistance));  //sends the distance of convex Hull
+            //session.getRemote().sendString(message); // and send it back
         }
 
 }
