5fa60377cf5321feb9601c95319c12fe13171af5
Silas Demez <silasdemez@gmail.com>
2021-02-03 21:55:18 +0100

    fixed main menu

--- main/src/Application.java
+++ main/src/Application.java
@@ -2,12 +2,13 @@
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
+import java.util.Scanner;
 
 import resources.*;
+
+import javax.swing.*;
 
 public class Application {
 
@@ -19,15 +20,19 @@ public class Application {
                 {200, 200.5617112013158,260.4274125362986, 0 }};
         String delimiter=";";
 
+        System.out.println("Suche den Algorithmus aus:\n1. NearestNeighbor\n2. Extended Nearestneighbor\n3. Beide\n\n");3
+
+        Scanner sc2 = new Scanner(System.in);
+        int eingabe = sc2.nextInt();
 
 
         //read from csv file
-        /*
+
         ReadCSV csvin = new ReadCSV(delimiter);
         csvin.setFilepath();
         csvin.readCmd();
         double[][] table = csvin.getdTable();
-         */
+
 
 
 
@@ -35,7 +40,7 @@ public class Application {
         //MappingMatrixGenerator matrixGenerator = new MappingMatrixGenerator(10);
         //double[][] table = matrixGenerator.generate();
 
-
+/*
         matrix = CoordinatesCalculator.executeCalculator(matrix);
         System.out.println(Arrays.deepToString(matrix));
 
@@ -50,22 +55,55 @@ public class Application {
 
         GrahamAlgorithmusV2.setNodes(points);
         System.out.println(GrahamAlgorithmusV2.computeConvexHull());
-/*
-        long startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
-        System.out.println("Start");
-
-        //NearestNeighbor
-        NearestNeighborV2 nearestNeighbor = new NearestNeighborV2();
-        //nearestNeighbor.executeNearestNeighbor(table,0);
-
-        //extendedNearestNeighbor
-        System.out.println(nearestNeighbor.extendedNearestNeighbor(matrix));     //prints the total distance
-
-        long endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
-
-        long duration = (endTime - startTime);
-        System.out.println("That took " + duration + " milliseconds");
 
  */
+
+        //long startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
+        //System.out.println("Start");
+
+
+
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println();
+
+
+
+        /*
+        String eingabe=JOptionPane.showInputDialog(f,"Pls Input your algorythm:");
+        int eingabe2= Integer.parseInt(eingabe);
+
+         */
+        System.out.println("Eingabe: " + eingabe);
+
+
+        //int eingabe = sc.nextInt();
+        //sc.close();
+        NearestNeighborV2 nearestNeighbor = new NearestNeighborV2();
+
+        switch (eingabe){
+            case 1:
+                table = nearestNeighbor.executeNearestNeighbor(table,0);
+                System.out.println("Diastance NearestNeighbor: " + nearestNeighbor.getDistance(table));
+            break;
+            case 2:
+                System.out.println("Distance extended NearestNeighbor" + nearestNeighbor.extendedNearestNeighbor(table));
+            break;
+            case 3:
+                table = nearestNeighbor.executeNearestNeighbor(table,0);
+                System.out.println("Diastance NearestNeighbor: " + nearestNeighbor.getDistance(table));
+                System.out.println("Distance extended NearestNeighbor" + nearestNeighbor.extendedNearestNeighbor(table));
+        }
+        //NearestNeighbor
+
+
+        //extendedNearestNeighbor
+        //System.out.println(nearestNeighbor.extendedNearestNeighbor(table));     //prints the total distance
+
+        //long endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
+
+        //long duration = (endTime - startTime);
+        //System.out.println("That took " + duration + " milliseconds");
+
+
     }
 }
