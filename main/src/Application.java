beb771c0b8baf02a5922d1fe20bcb1cc06a9e50b
Silas Demez <silasdemez@gmail.com>
2021-02-04 21:58:27 +0100

    added menu

--- main/src/Application.java
+++ main/src/Application.java
@@ -2,8 +2,12 @@
 import resources.GrahamAlgorithmusV2;
 
 import java.awt.geom.Point2D;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import resources.*;
 
 /**
  * The application to run the project
@@ -15,47 +19,77 @@ public class Application {
      * @param args manual input
      */
     public static void main(String[] args) {
-        double[][] matrix = {{0, 15, 64, 200},
-                {15, 0, 60, 200.5617112013158},
-                {64, 60, 0, 260.4274125362986},
-                {200, 200.5617112013158,260.4274125362986, 0 }};
+        long startTime, endTime, duration;
+
+
         String delimiter=";";
 
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Suche den Algorithmus aus:\n 1) NearestNeighbor\n 2) Extended NearestNeighbor\n 3) Convex Hull (Graham Algorithmus)\n\n");
+        int eingabe = scanner.nextInt();
 
         //read from csv file
-        /*
         ReadCSV csvin = new ReadCSV(delimiter);
         csvin.setFilepath();
         csvin.readCmd();
-        double[][] table = csvin.getdTable();
-         */
-
-
+        double[][] table1 = csvin.getdTable();
+        double[][] table = {{0, 15, 64, 200},
+                {15, 0, 60, 200.5617112013158},
+                {64, 60, 0, 260.4274125362986},
+                {200, 200.5617112013158,260.4274125362986, 0 }};
 
         //generate adjacency matrix in java
         //MappingMatrixGenerator matrixGenerator = new MappingMatrixGenerator(10);
         //double[][] table = matrixGenerator.generate();
 
+        switch (eingabe){
+            case 1:
+                startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
+                NearestNeighborV3.executeNearestNeighbor(table,0);
+                endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
+                duration = (endTime - startTime);
+                System.out.println("That took " + duration + " milliseconds");
+                System.out.println("Executed Nearest Neighbor");
+            break;
+            case 2:
+                startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
+                NearestNeighborV3.extendedNearestNeighborV2(table);
+                endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
+                duration = (endTime - startTime);
+                System.out.println("That took " + duration + " milliseconds");
+                System.out.println("Executed extended Nearest Neighbor");
+            break;
+            case 3:
+                startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
+                table = CoordinatesCalculator.executeCalculator(table);
 
-        //matrix = CoordinatesCalculator.executeCalculator(matrix);
-        //System.out.println(Arrays.deepToString(matrix));
+                List<Point2D> points = new ArrayList<>();
 
+                System.out.println(table.length);
 
-        List<Point2D> points = new ArrayList<>();
+                for (int i=0;i<table.length;i++) {
+                    points.add(i, new Point2D.Double(table[i][0], table[i][1]));
+                }
+                System.out.println(points);
 
-        for (int i=0;i < matrix.length;i++) {
-            points.add(i, new Point2D.Double(matrix[i][0], matrix[i][1]));
+                GrahamAlgorithmusV2.setNodes(points);
+                GrahamAlgorithmusV2.computeConvexHull();
+                endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
+                duration = (endTime - startTime);
+                System.out.println("That took " + duration + " milliseconds");
+                System.out.println("Executed Convex Hull (Graham Algorithmus)");
+            break;
+            default:
+                System.out.println("Da hast du wohl etwas falsch eingegeben");
+            break;
         }
-        System.out.println(points);
 
-        GrahamAlgorithmusV2.setNodes(points);
-        System.out.println(GrahamAlgorithmusV2.computeConvexHull());
+
 
         //GrahamAlgorithmusV2.setNodes(points);
         //System.out.println(GrahamAlgorithmusV2.computeConvexHull());
 /*
-        long startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
+
         System.out.println("Start");
 
         //NearestNeighbor
@@ -65,9 +99,7 @@ public class Application {
         //extendedNearestNeighbor
         System.out.println(nearestNeighbor.extendedNearestNeighbor(matrix));     //prints the total distance
 
-        long endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
 
-        long duration = (endTime - startTime);
         System.out.println("That took " + duration + " milliseconds");
 
  */
