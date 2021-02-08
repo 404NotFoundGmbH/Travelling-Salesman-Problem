cf49a934fdb779e2c38b0df27e3937e88495263c
Silas Demez <silasdemez@gmail.com>
2021-02-08 15:25:31 +0100

    changed from demonstration setup to execution setup

--- main/src/Application.java
+++ main/src/Application.java
@@ -1,4 +1,3 @@
-
 import resources.*;
 
 import java.awt.geom.Point2D;
@@ -30,46 +29,40 @@ public class Application {
         ReadCSV csvin = new ReadCSV(delimiter);
         csvin.setFilepath();
         csvin.readCmd();
-        double[][] table1 = csvin.getdTable();
-        double[][] table = {{0, 15, 64, 200},
-                {15, 0, 60, 200.5617112013158},
-                {64, 60, 0, 260.4274125362986},
-                {200, 200.5617112013158,260.4274125362986, 0 }};
+        double[][] table = csvin.getdTable();
 
-        //generate adjacency matrix in java
-        //MappingMatrixGenerator matrixGenerator = new MappingMatrixGenerator(10);
-        //double[][] table = matrixGenerator.generate();
+        /*
+        generate adjacency matrix in java
+        MappingMatrixGenerator matrixGenerator = new MappingMatrixGenerator(10);
+        double[][] table = matrixGenerator.generate();
+         */
 
-        switch (eingabe){
-            case 1:
+        switch (eingabe) {
+            case 1 -> {     // Nearest Neighbor
                 startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
-                System.out.println("Distance: " + NearestNeighborV3.executeNearestNeighbor(table1,0));
+                System.out.println("Distance: " + NearestNeighborV3.executeNearestNeighbor(table, 0));
                 endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
                 duration = (endTime - startTime);
                 System.out.println("That took " + duration + " milliseconds");
                 System.out.println("Executed Nearest Neighbor");
-            break;
-            case 2:
+            }
+            case 2 -> {     // Extended Nearest Neighbor
                 startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
-                System.out.println("Distance: " + NearestNeighborV3.extendedNearestNeighborV2(table1));
+                System.out.println("Distance: " + NearestNeighborV3.extendedNearestNeighborV2(table));
                 endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
                 duration = (endTime - startTime);
                 System.out.println("That took " + duration + " milliseconds");
                 System.out.println("Executed extended Nearest Neighbor");
-            break;
-            case 3:
+            }
+            case 3 -> {     // Convex Hull
                 startTime = Instant.now().toEpochMilli();      //get current time - used to measure execution time
                 table = CoordinatesCalculator.executeCalculator(table);
-
                 List<Point2D> points = new ArrayList<>();
-
                 System.out.println(table.length);
-
-                for (int i=0;i<table.length;i++) {
+                for (int i = 0; i < table.length; i++) {
                     points.add(i, new Point2D.Double(table[i][0], table[i][1]));
                 }
                 System.out.println(points);
-
                 GrahamAlgorithmusV2.setNodes(points);
                 GrahamAlgorithmusV2.computeConvexHull();
                 endTime = Instant.now().toEpochMilli();    //get current time - used to measure execution time
@@ -77,10 +70,8 @@ public class Application {
                 System.out.println("Distance: " + GrahamAlgorithmusV2.getFinalDistance());
                 System.out.println("That took " + duration + " milliseconds");
                 System.out.println("Executed Convex Hull (Graham Algorithmus)");
-            break;
-            default:
-                System.out.println("Please check your entry and try again!");
-            break;
+            }
+            default -> System.out.println("Please check your entry and try again!");
         }
     }
 }
